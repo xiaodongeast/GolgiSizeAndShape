235c382ac4badbc0ef1b8b212063ab2abfc359e9
xiaodongeast <32018338+xiaodongeast@users.noreply.github.com>
2019-09-20 22:17:57 -0500

    helper for the main code

--- GolgiCentro/CalculateK.m
+++ GolgiCentro/CalculateK.m
@@ -1,21 +1,25 @@
-function [ out ] = CalculateK( Dstage2, bin )
+function [ out ] = CalculateK( Dstage2, bin,xyScale,zScale )
+% xyScale and zScale is only for the Golgi px
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-xyScale=0.238;
-zScale=0.8;
+
+%xyScale=0.238;
+%zScale=0.8;
 meanDis=0;
 out=struct;
 
+xyScale2=1;
+zScale2=1;
 % for each time point get centrosome matrix, Golgi matrix
 for i=1:height(Dstage2)
-    centr=Dstage2.centroCor(i).value;
+    centr=Dstage2.center_centrosome(i).value;
     Golgi=Dstage2.GolgiCor(i).data(:,2:4);
     out(i).time=Dstage2.timePoint(i);
     out(i).bin=bin;
     [cm,cn]=size(centr);
     [gm,gn]=size(Golgi);
 Golgi=[Golgi(:,1)*xyScale,Golgi(:,2)*xyScale,Golgi(:,3)*zScale];
-centr=[centr(:,1)*xyScale,centr(:,2)*xyScale,centr(:,3)*zScale]; 
+centr=[centr(:,1)*xyScale2,centr(:,2)*xyScale2,centr(:,3)*zScale2]; 
 % if the number of the centrosome is 1 get the distance
 
 d=zeros(gm,1);
@@ -26,6 +30,7 @@ if cm==1
 end   
 % if the number of the centrosome is 2 get the min distance
 if cm==2
+    
     for j=1:gm
         d1=disK(centr(1,:),Golgi(j,:));
         d2=disK(centr(2,:),Golgi(j,:));
